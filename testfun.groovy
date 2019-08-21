74a4d07846168fdbf29037b2d4e38bbb945574c4
mani1soni <manish.soni8403@gmail.com>
2019-08-21 18:21:30 +0530

    Jenkins file updated

--- testfun.groovy
+++ testfun.groovy
@@ -1,13 +1,15 @@
 #!/usr/bin/env groovy
 import hudson.model.*
-def call(REPO_PATH,APP_NAME){
-    stages{
-        stage('build and deploy'){
-            steps{
-                sh '''
-                cd REPO_PATH/APP_NAME/
-                mkdir testing
+node('master'){
+    def call(REPO_PATH,APP_NAME){
+        stages{
+            stage('build and deploy'){
+                steps{
+                    sh '''
+                    cd REPO_PATH/APP_NAME/
+                    mkdir testing
                     '''
+                }
             }
         }
     }
