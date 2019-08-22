6cb9a31320aeb5c843fb585b35418a5370a2030e
mani1soni <manish.soni8403@gmail.com>
2019-08-22 14:42:09 +0530

    Jenkinsfile updated

--- testfun.groovy
+++ testfun.groovy
@@ -1,10 +1,22 @@
 #!/usr/bin/env groovy
-def call(REPO_PATH,APP_NAME){
+//def call(REPO_PATH,APP_NAME){
+//    stages{
+//        stage('build and deploy'){
+//            steps{
+//                sh '''
+//                cd REPO_PATH/APP_NAME/
+//                mkdir testing
+//                '''
+//            }
+//        }
+//    }
+//}
+def call(){
     stages{
         stage('build and deploy'){
             steps{
                 sh '''
-                cd REPO_PATH/APP_NAME/
+                cd /home/manish/Desktop/test/
                 mkdir testing
                 '''
             }
