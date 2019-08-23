57e70782f74bfbc27725c38fd1ec9508e8265272
mani1soni <manish.soni8403@gmail.com>
2019-08-23 13:45:10 +0530

    groovy file updated

--- testfun.groovy
+++ testfun.groovy
@@ -2,11 +2,11 @@
 def call(def path,def app){
     return{
         stage('build and deploy'){
-                sh''' 
-                cd ${path}/${app}/
-                mkdir testing
-                '''
-            }
+            sh''' 
+            cd ${path}/${app}/
+            mkdir testing
+            '''
         }
     }
 }
+
