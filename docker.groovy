00eddfecdad4386aa0a49ec55a80077995d7807c
mani1soni <manish.soni8403@gmail.com>
2019-08-26 15:59:11 +0530

    docker function file updated

--- docker.groovy
+++ docker.groovy
@@ -1,4 +1,9 @@
 #!/usr/bin/env groovy
 def call(CONTAINER,IMAGE){
-    sh 'docker run -d --name ${CONTAINER} ${IMAGE}'
-} 
+    stage('docker deploy'){
+        sh 'docker run -d --name ${CONTAINER} ${IMAGE}'
+    }
+}
+return [
+    call: this.&call
+]
