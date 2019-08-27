7dcb950bceb302076931fbc775b941d1b713de61
mani1soni <manish.soni8403@gmail.com>
2019-08-27 10:48:58 +0530

    jenkinsfile updated

--- docker.groovy
+++ docker.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call(CONTAINER,IMAGE){
-   sh 'sudo docker run -itd --name ${CONTAINER} ${IMAGE}'
+   sh "sudo docker run -itd --name '${CONTAINER}' '${IMAGE}'"
 }
 return this
 
